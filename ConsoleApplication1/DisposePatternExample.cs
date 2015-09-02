072e265a17d61d7e752a441eb6bda7dd17b0c53f
shmitov <shmitov@moedelo.org>
2015-09-03 00:48:57 +0300

    DisposePatternExample

---
+++ ConsoleApplication1/DisposePatternExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class DisposePatternExample : IDisposable
+    {
+        bool _disposed = false;
+        private IntPtr _umResource;
+        private const string InstanceName = "DisposePatternExample";
+
+        public DisposePatternExample()
+        {
+            _umResource = Marshal.StringToCoTaskMemAuto(InstanceName);
+        }
+
+
+        // Flag: Has Dispose already been called?
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //TODO://вопрос на засыпку, наши managed сlass`es типа Stream и тд которые 
+        //TODO://инкапсулируют unmanaged ресурсы и имплементят IDispose() в какой блок кода надо поместить ? :)
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                
+            }
+
+            // Free any unmanaged objects here.
+            //
+
+            if (_umResource != IntPtr.Zero)
+            {
+                Marshal.FreeCoTaskMem(_umResource);
+                Console.WriteLine("[{0}] Unmanaged memory freed at {1:x16}",
+                    InstanceName, _umResource.ToInt64());
+                _umResource = IntPtr.Zero;
+            }
+
+            _disposed = true;
+        }
+
+        ~DisposePatternExample()
+        {
+            Dispose(false);
+        }
+    }
+}
